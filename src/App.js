bf05276000aea66f4bd41e4c00781aa8a32d802a
chasetreadwell <chasetreadwell7@gmail.com>
2020-09-26 15:49:05 -0600

    Added Layout,js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { Layout } from './components/Layout';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-
+        <Layout>
+          <h1>How are you today?</h1>
+        </Layout>
       </React.Fragment>
     )
   }
