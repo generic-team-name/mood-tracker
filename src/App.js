efd805a3d54c19bdd4d63752907ed60b648f9331
chasetreadwell <chasetreadwell7@gmail.com>
2020-09-26 20:42:35 -0600

    Add history framework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Container } from 'react-bootstrap'
 import { Layout } from './components/Layout';
 import SubmitForm from './components/Form';
+import History from './components/History';
 
 class App extends Component {
   render() {
@@ -9,7 +11,12 @@ class App extends Component {
         <Layout>
           <br />
           <h1>How are you today?</h1>
+          <Container fluid>
           <SubmitForm />
+          </Container>
+          <Container fluid>
+          <History />
+          </Container>
         </Layout>
       </React.Fragment>
     )
---
+++ src/components/History.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class History extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return null
+    }
+}
