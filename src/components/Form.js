349366368581eb8ba3753cb928ce8b50a6ac66a2
chasetreadwell <chasetreadwell7@gmail.com>
2020-09-26 21:38:58 -0600

    Add icons

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,8 +25,7 @@ export default class SubmitForm extends Component {
         const entry = this.state.entry;
         alert('This was submitted: ' + mood + entry);
         event.preventDefault();
-        let dirtyA = localStorage.getItem('array')
-        let a = JSON.parse(dirtyA);
+        let a = JSON.parse(localStorage.getItem('array'))
         a.unshift([Date.now(), parseInt(mood), entry]);
         console.log(a);
         localStorage.setItem('array', JSON.stringify(a));
@@ -36,13 +35,14 @@ export default class SubmitForm extends Component {
         return (
             <Fo onSubmit={this.handleSubmit}>
                 <Fo.Row>
-                    <ToggleButtonGroup exclusive="true" name="mood" toggle size="md">
-                        <ToggleButton type="radio" value="5" variant="success" onChange={this.handleChange}>Great</ToggleButton>
-                        <ToggleButton type="radio" value="4" variant="primary" onChange={this.handleChange}>Good</ToggleButton>
-                        <ToggleButton type="radio" value="3" variant="info" onChange={this.handleChange}>Okay</ToggleButton>
-                        <ToggleButton type="radio" value="2" variant="warning" onChange={this.handleChange}>Bad</ToggleButton>
-                        <ToggleButton type="radio" value="1" variant="danger" onChange={this.handleChange}>Terrible</ToggleButton>
+                    <ToggleButtonGroup exclusive="true" name="mood" toggle size="lg">
+                        <ToggleButton type="radio" value="5" variant="success" onChange={this.handleChange}><img alt='Great' src='../assets/png/great.png'/></ToggleButton>
+                        <ToggleButton type="radio" value="4" variant="primary" onChange={this.handleChange}><img alt='Good' src='../assets/png/good.png'/></ToggleButton>
+                        <ToggleButton type="radio" value="3" variant="info" onChange={this.handleChange}><img alt='Okay' src='../assets/png/okay.png'/></ToggleButton>
+                        <ToggleButton type="radio" value="2" variant="warning" onChange={this.handleChange}><img alt='Bad' src='../assets/png/bad.png'/></ToggleButton>
+                        <ToggleButton type="radio" value="1" variant="danger" onChange={this.handleChange}><img alt='Terrible' src='../assets/png/terrible.png'/></ToggleButton>
                     </ToggleButtonGroup>
+                    <h6>Icons made by <a href="https://www.flaticon.local/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.local/" title="Flaticon">www.flaticon.local</a></h6>
                 </Fo.Row>
                 <br />
                 <Fo.Row>
